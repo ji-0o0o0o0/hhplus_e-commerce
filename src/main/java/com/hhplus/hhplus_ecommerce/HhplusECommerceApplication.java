052f9035609c62914252671ccb17c66ba3643ab6
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-10-29 15:56:14 +0900

    init commit

---
+++ src/main/java/com/hhplus/hhplus_ecommerce/HhplusECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusECommerceApplication.class, args);
+    }
+
+}
